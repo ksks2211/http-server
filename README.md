b5cfa27810e21a5dfab9ca11a1cc5c5e438e2548
ksks2211 <44663376+ksks2211@users.noreply.github.com>
2021-03-01 18:24:52 +0900

    update css and html

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # http-server
 simple RESTful server only using http module on node.js
+
+### screenshot
+![screenshot](photo/screenshot.png)
--- restFront.js
+++ restFront.js
@@ -2,13 +2,21 @@ async function getUser(){  // get user
     try{
         const res = await axios.get('/users');
         const users = res.data;
-        const list = document.getElementById('list');
+        const list = document.getElementById('table-list');
         list.innerHTML = '';
 
         Object.keys(users).map(function(key){
-            const span = document.createElement('span');
-            span.textContent = users[key];
+            const tr = document.createElement('tr');
+            
+            const td1 = document.createElement('td');
+            const td2 = document.createElement('td');
+            const td3 = document.createElement('td');
 
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+
+            td1.textContent = users[key];
 
             //Edit button
             const edit = document.createElement('button');
@@ -38,12 +46,10 @@ async function getUser(){  // get user
                 }
             })
 
-            const userDiv = document.createElement('div');
-            userDiv.appendChild(span);
-            userDiv.appendChild(edit);
-            userDiv.appendChild(remove);
+            td2.appendChild(edit);
+            td3.appendChild(remove);
 
-            list.appendChild(userDiv);
+            list.appendChild(tr);
 
             //console.log(res.data[key]);
         })
